e075df81f67b3270952ea0c263ce16e4f5aef0da
thenn <thenn@onepay.vn>
2023-02-26 18:29:02 +0700

    init

---
+++ src/main/java/com/vn/ecommerservice/EcommerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vn.ecommerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerServiceApplication.class, args);
+    }
+
+}
